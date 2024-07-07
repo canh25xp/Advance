c04f6d69b37bab8b44b18785bf2b20a839b2cc2b
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-07 19:42:59 +0700

    rename

---
+++ Point_of_Balance_2/Source.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+using namespace std;
+    
+int main(int argc, char* argv[]){
+    const char* inputfile = argv[1];
+    freopen(inputfile, "r", stdin);
+    
+
+    return 0;
+}
