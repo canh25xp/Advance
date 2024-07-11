7c83d761c29c3a7f330c713e68f29ee6f4b5b208
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-11 22:45:09 +0700

    point.hpp header only

--- shared/Point/point.hpp
+++ shared/Point/point.hpp
@@ -1,5 +1,6 @@
-#pragma once
 struct Point {
+    int x, y;
+
     Point();
     Point(int x, int y);
 
@@ -16,5 +17,58 @@ struct Point {
     void operator-=(const int &rhs);
     void operator--();
 
-    int x, y;
 };
+
+Point::Point() : x(0), y(0) {}
+
+Point::Point(int x, int y) : x(x), y(y) {}
+
+bool Point::operator==(const Point &rhs) const {
+    if (this->x == rhs.x && this->y == rhs.y)
+        return true;
+    return false;
+}
+
+bool Point::operator!=(const Point &rhs) const {
+    if (this->x == rhs.x && this->y == rhs.y)
+        return false;
+    return true;
+}
+
+void Point::operator+=(const Point &rhs) {
+    this->x += rhs.x;
+    this->y += rhs.y;
+}
+
+void Point::operator+=(const int &rhs) {
+    this->x += rhs;
+    this->y += rhs;
+}
+
+void Point::operator++() {
+    this->x++;
+    this->y++;
+}
+
+void Point::operator-=(const Point &rhs) {
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+}
+
+void Point::operator-=(const int &rhs) {
+    this->x -= rhs;
+    this->y -= rhs;
+}
+
+Point Point::operator+(const Point &rhs) const {
+    return Point(this->x + rhs.x, this->y + rhs.y);
+}
+
+Point Point::operator-(const Point &rhs) const {
+    return Point(this->x - rhs.x, this->y - rhs.y);
+}
+
+void Point::operator--() {
+    this->x--;
+    this->y--;
+}
