22e92a21b8a8a63aa5f94bdc8067cbb1ecf2eea4
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-11 21:26:49 +0700

    format code

--- Uniform_Distribution/main.cpp
+++ Uniform_Distribution/main.cpp
@@ -7,156 +7,158 @@ using namespace std;
 
 #define MAX_SIZE 5
 
-template <class T> class Stack {
+template <class T>
+class Stack {
 public:
-	Stack();
+    Stack();
 
-	bool push(T k);
-	T pop();
-	T topElement();
-	bool isFull();
-	bool isEmpty();
+    bool push(T k);
+    T pop();
+    T topElement();
+    bool isFull();
+    bool isEmpty();
+
 private:
-	int top;
+    int top;
 
-	T st[STACK_SIZE];
+    T st[STACK_SIZE];
 };
 
-struct Pair{
-	Pair();
-	Pair(int x, int y);
-	int x;
-	int y;
+struct Pair {
+    Pair();
+    Pair(int x, int y);
+    int x;
+    int y;
 };
 
-template<int max_size>
-int contiguous_count(const int(&matrix)[max_size][max_size], const int& value, const int& size);
+template <int max_size>
+int contiguous_count(const int (&matrix)[max_size][max_size], const int &value, const int &size);
 
 const int dx[] = {1, -1, 0, 0};
 const int dy[] = {0, 0, 1, -1};
 
 int main(int argc, char **argv) {
-  const char *input = (argc > 1) ? argv[1] : "input.txt";
-  freopen(input, "r", stdin);
+    const char *input = (argc > 1) ? argv[1] : "input.txt";
+    freopen(input, "r", stdin);
 
-	int TEST_CASE = DEFAULT_TEST_CASE;
-	cin >> TEST_CASE;
-	for(int case_index = 0 ; case_index <TEST_CASE ; case_index++){
-		int N = 0;
-		cin >> N;
+    int TEST_CASE = DEFAULT_TEST_CASE;
+    cin >> TEST_CASE;
+    for (int case_index = 0; case_index < TEST_CASE; case_index++) {
+        int N = 0;
+        cin >> N;
 
-		int matrix[MAX_SIZE][MAX_SIZE] = {};
+        int matrix[MAX_SIZE][MAX_SIZE] = {};
 
-		for (int i = 0; i < N-1; i++){
-			for (int j = 0; j < N; j++){
-				int x, y;
-				cin >> x >> y;
-				matrix[x-1][y-1] = i+1;
-			}
-		}
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N; j++) {
+                int x, y;
+                cin >> x >> y;
+                matrix[x - 1][y - 1] = i + 1;
+            }
+        }
 
-		int flag = true;
+        int flag = true;
 
-		for (int i = 0; i < N; i++){
-			int count = contiguous_count<MAX_SIZE>(matrix,i, N);
-			if(count < N){
-				flag = false;
-				break;
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            int count = contiguous_count<MAX_SIZE>(matrix, i, N);
+            if (count < N) {
+                flag = false;
+                break;
+            }
+        }
 
-		cout<< "Case #" << case_index+1 << endl;
-		if(flag)
-			cout << "good" << endl;
-		else
-			cout << "wrong" << endl;
-	}
+        cout << "Case #" << case_index + 1 << endl;
+        if (flag)
+            cout << "good" << endl;
+        else
+            cout << "wrong" << endl;
+    }
 
-	fclose(input);
+    fclose(input);
 
-	return 0;
+    return 0;
 }
 
-template<int max_size>
-int contiguous_count(const int(&matrix)[max_size][max_size], const int& value, const int& size){
-	bool checked[max_size][max_size] = {};
-	int count = 0;
-	for (int i = 0; i < size; i++){
-		for (int j = 0; j < size; j++){
-			if(matrix[i][j] == value){
-				Stack<Pair> stack;
-				Pair pair(i,j);
-				stack.push(pair);
-				checked[i][j] = true;
-				count++;
-				while(!stack.isEmpty()){
-					pair = stack.pop();
-					for (int d = 0; d < 4; d++){
-						Pair new_pair(pair.x+dx[d], pair.y+dy[d]);
-						if(new_pair.x >= 0 && new_pair.x < size && new_pair.y >= 0 && new_pair.y < size && matrix[new_pair.x][new_pair.y] == value && checked[new_pair.x][new_pair.y] == 0){
-							stack.push(new_pair);
-							checked[new_pair.x][new_pair.y] = 1;
-							count++;
-						}
-					}
-				}
-				return count;
-			}
-		}
-	}
-	return count;
+template <int max_size>
+int contiguous_count(const int (&matrix)[max_size][max_size], const int &value, const int &size) {
+    bool checked[max_size][max_size] = {};
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (matrix[i][j] == value) {
+                Stack<Pair> stack;
+                Pair pair(i, j);
+                stack.push(pair);
+                checked[i][j] = true;
+                count++;
+                while (!stack.isEmpty()) {
+                    pair = stack.pop();
+                    for (int d = 0; d < 4; d++) {
+                        Pair new_pair(pair.x + dx[d], pair.y + dy[d]);
+                        if (new_pair.x >= 0 && new_pair.x < size && new_pair.y >= 0 && new_pair.y < size && matrix[new_pair.x][new_pair.y] == value && checked[new_pair.x][new_pair.y] == 0) {
+                            stack.push(new_pair);
+                            checked[new_pair.x][new_pair.y] = 1;
+                            count++;
+                        }
+                    }
+                }
+                return count;
+            }
+        }
+    }
+    return count;
 }
 
 template <class T>
 Stack<T>::Stack() {
-	top = -1; 
+    top = -1;
 }
 
 template <class T>
-bool Stack<T>::push(T k){
-	if (isFull()) {
-		return 1;
-	}
-	top++;
-	st[top] = k;
-	return 0;
+bool Stack<T>::push(T k) {
+    if (isFull()) {
+        return 1;
+    }
+    top++;
+    st[top] = k;
+    return 0;
 }
 
 template <class T>
-bool Stack<T>::isEmpty(){
-	if (top == -1)
-		return 1;
-	else
-		return 0;
+bool Stack<T>::isEmpty() {
+    if (top == -1)
+        return 1;
+    else
+        return 0;
 }
 
 template <class T>
-bool Stack<T>::isFull(){
-	if (top == (STACK_SIZE - 1))
-		return 1;
-	else
-		return 0;
+bool Stack<T>::isFull() {
+    if (top == (STACK_SIZE - 1))
+        return 1;
+    else
+        return 0;
 }
 
 template <class T>
-T Stack<T>::pop(){
-	T popped_element = st[top];
-	top--;
-	return popped_element;
+T Stack<T>::pop() {
+    T popped_element = st[top];
+    top--;
+    return popped_element;
 }
 
 template <class T>
-T Stack<T>::topElement(){
-	T top_element = st[top];
-	return top_element;
+T Stack<T>::topElement() {
+    T top_element = st[top];
+    return top_element;
 }
 
-Pair::Pair(){
-	x=0;
-	y=0;
+Pair::Pair() {
+    x = 0;
+    y = 0;
 }
 
-Pair::Pair(int _x, int _y){
-	x = _x;
-	y= _y;
+Pair::Pair(int _x, int _y) {
+    x = _x;
+    y = _y;
 }
