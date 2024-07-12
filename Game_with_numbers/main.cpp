02a775fc6d1887362f612bfd07caa0ea96a9dc6c
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-12 22:12:29 +0700

    fotmat code

--- Game_with_numbers/main.cpp
+++ Game_with_numbers/main.cpp
@@ -1,30 +1,30 @@
-#define _CRT_SECURE_NO_WARNINGS // Suppress Warning	C4996: 'freopen': This function or variable may be unsafe. Consider using freopen_s instead.
 #include <iostream>
 using namespace std;
 
-#define MIN(a,b) ((a)<(b)?(a):(b))
-#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 const int MAX_N = 4; // Should be 30
 
+int MinHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j);
+int MaxHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j);
 
-int MinHard(const int (&num)[MAX_N], int(&hard)[MAX_N][MAX_N], int i, int j);
-int MaxHard(const int (&num)[MAX_N], int(&hard)[MAX_N][MAX_N], int i, int j);
-
-int main(){
+int main() {
     freopen("eval_input_small.txt", "r", stdin);
-    int T; cin >> T;
-    for (int tc = 0; tc < T; tc++){
-        int N; cin >> N;
+    int T;
+    cin >> T;
+    for (int tc = 0; tc < T; tc++) {
+        int N;
+        cin >> N;
         int num[MAX_N] = {};
-        for (int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             cin >> num[i];
         }
 
         int hard[MAX_N][MAX_N] = {};
 
-        int minHard = MinHard(num, hard, 0, N-1);
-        int maxHard = MaxHard(num, hard, 0, N-1);
+        int minHard = MinHard(num, hard, 0, N - 1);
+        int maxHard = MaxHard(num, hard, 0, N - 1);
 
         cout << "Case #" << tc + 1 << endl;
         cout << minHard << " " << maxHard << endl;
@@ -32,21 +32,21 @@ int main(){
     return 0;
 }
 
-int MinHard(const int (&num)[MAX_N], int(&hard)[MAX_N][MAX_N], int i, int j){
+int MinHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j) {
     return 0;
 }
 
-int MaxHard(const int (&num)[MAX_N], int(&hard)[MAX_N][MAX_N], int i, int j){
+int MaxHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j) {
     if (i > j)
         return 0;
 
     if (hard[i][j])
         return hard[i][j];
 
-    int s1 = num[i] + MaxHard(num, hard, i+1, j-1);
-    int s2 = num[i] + MaxHard(num, hard, i+2, j  );
-    int s3 = num[j] + MaxHard(num, hard, i+1, j-1);
-    int s4 = num[j] + MaxHard(num, hard, i  , j-2);
+    int s1 = num[i] + MaxHard(num, hard, i + 1, j - 1);
+    int s2 = num[i] + MaxHard(num, hard, i + 2, j);
+    int s3 = num[j] + MaxHard(num, hard, i + 1, j - 1);
+    int s4 = num[j] + MaxHard(num, hard, i, j - 2);
 
     hard[i][j] = MAX(MIN(s1, s2), MIN(s3, s4));
 
--- Game_with_numbers/solution.cpp
+++ Game_with_numbers/solution.cpp
@@ -5,93 +5,97 @@ using namespace std;
 #define USER 0
 #define COM 1
 
-int num[MAX_N]; 
- 
-struct score
-{
-	int user;
-	int com;
-	score(){
-		user = 0;
-		com = 0;
-	}
-	void init(){
-		user = 0;
-		com = 0;
-	}
-	bool valid(){
-		if(user == 0) return false;
-		return true;
-	}
+int num[MAX_N];
+
+struct score {
+    int user;
+    int com;
+    score() {
+        user = 0;
+        com = 0;
+    }
+    void init() {
+        user = 0;
+        com = 0;
+    }
+    bool valid() {
+        if (user == 0)
+            return false;
+        return true;
+    }
 };
 
 score easy[MAX_N][MAX_N];
 score hard[MAX_N][MAX_N];
 
-score Max(score a, score b, int turn){
-	if(turn == USER)
-		return a.user > b.user ? a : b;
-	return a.com > b.com ? a : b;
+score Max(score a, score b, int turn) {
+    if (turn == USER)
+        return a.user > b.user ? a : b;
+    return a.com > b.com ? a : b;
 }
 
-score Min(score a, score b, int turn){
-	if(turn == USER)
-		return a.user < b.user ? a : b;
-	return a.com < b.com ? a : b;
+score Min(score a, score b, int turn) {
+    if (turn == USER)
+        return a.user < b.user ? a : b;
+    return a.com < b.com ? a : b;
 }
 
-
-score playHard(int i, int j, int turn){
-	if (i > j)
-		return score();
-	if (hard[i][j].valid())
-		return hard[i][j];
-	if(turn == USER){
-		score sumL = playHard(i + 1, j, COM);  sumL.user += num[i];
-		score sumR = playHard(i, j - 1, COM);  sumR.user += num[j];
-		return hard[i][j] = Max(sumL,sumR,USER);
-	}else if(turn == COM)
-	{
-		score sumL = playHard(i + 1, j, USER);  sumL.com += num[i];
-		score sumR = playHard(i, j - 1, USER);  sumR.com += num[j];
-		return hard[i][j] = Max(sumL,sumR,COM);
-	}
+score playHard(int i, int j, int turn) {
+    if (i > j)
+        return score();
+    if (hard[i][j].valid())
+        return hard[i][j];
+    if (turn == USER) {
+        score sumL = playHard(i + 1, j, COM);
+        sumL.user += num[i];
+        score sumR = playHard(i, j - 1, COM);
+        sumR.user += num[j];
+        return hard[i][j] = Max(sumL, sumR, USER);
+    } else if (turn == COM) {
+        score sumL = playHard(i + 1, j, USER);
+        sumL.com += num[i];
+        score sumR = playHard(i, j - 1, USER);
+        sumR.com += num[j];
+        return hard[i][j] = Max(sumL, sumR, COM);
+    }
 }
 
-score playEasy(int i, int j, int turn){
-	if (i > j)
-		return score();
-	if (easy[i][j].valid())
-		return easy[i][j];
-	if(turn == USER){
-		score sumL = playEasy(i + 1, j, COM); sumL.user += num[i];
-		score sumR = playEasy(i, j - 1, COM); sumR.user += num[j];
-		return easy[i][j] = Max(sumL,sumR,USER);
-	}else if(turn == COM)
-	{
-		score sumL = playEasy(i + 1, j, USER); sumL.com += num[i];
-		score sumR = playEasy(i, j - 1, USER); sumR.com += num[j];
-		return Min(sumL,sumR,COM);
-	}
+score playEasy(int i, int j, int turn) {
+    if (i > j)
+        return score();
+    if (easy[i][j].valid())
+        return easy[i][j];
+    if (turn == USER) {
+        score sumL = playEasy(i + 1, j, COM);
+        sumL.user += num[i];
+        score sumR = playEasy(i, j - 1, COM);
+        sumR.user += num[j];
+        return easy[i][j] = Max(sumL, sumR, USER);
+    } else if (turn == COM) {
+        score sumL = playEasy(i + 1, j, USER);
+        sumL.com += num[i];
+        score sumR = playEasy(i, j - 1, USER);
+        sumR.com += num[j];
+        return Min(sumL, sumR, COM);
+    }
 }
 
-int main(void)
-{
-	int test_case;
-	int T, N;
-	freopen("Text.txt", "r", stdin);
-	cin >> T;
+int main(void) {
+    int test_case;
+    int T, N;
+    freopen("Text.txt", "r", stdin);
+    cin >> T;
 
-	for (test_case = 1; test_case <= T; ++test_case)
-	{
-		cin >> N;
-		for (int i = 0; i < N; i++) {
-			cin >> num[i];
-			for (int j = 0; j < N; j++){
-				hard[i][j].init(); easy[i][j].init();
-			}
-		}
-		printf("Case #%d\n%d %d\n", test_case, playEasy(0, N - 1,0).user, playHard(0, N - 1,0).user);
-	}
-	return 0;
+    for (test_case = 1; test_case <= T; ++test_case) {
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            cin >> num[i];
+            for (int j = 0; j < N; j++) {
+                hard[i][j].init();
+                easy[i][j].init();
+            }
+        }
+        printf("Case #%d\n%d %d\n", test_case, playEasy(0, N - 1, 0).user, playHard(0, N - 1, 0).user);
+    }
+    return 0;
 }
