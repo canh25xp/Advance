ea8f88daa14ba9c49873ea4f9dce9d2e370703d9
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-12 22:05:01 +0700

    format code

--- Bridge/main.cpp
+++ Bridge/main.cpp
@@ -1,24 +1,24 @@
-#define _CRT_SECURE_NO_WARNINGS // Suppress Warning	C4996: 'freopen': This function or variable may be unsafe. Consider using freopen_s instead.
+#define _CRT_SECURE_NO_WARNINGS // Suppress Warning C4996: 'freopen': This function or variable may be unsafe. Consider using freopen_s instead.
 #include <iostream>
 using namespace std;
 
-const int MAX_W = 5; // Width of the bridge should always be 5
+const int MAX_W = 5;  // Width of the bridge should always be 5
 const int MAX_L = 20; // Length of the bridge should be no more than 20
 
 struct Point {
     Point();
     Point(int _x, int _y);
 
-    bool operator==(const Point& rhs) const;
-    bool operator!=(const Point& rhs) const;
-    void operator+=(const Point& rhs);
-    Point operator+(const Point& rhs) const;
+    bool operator==(const Point &rhs) const;
+    bool operator!=(const Point &rhs) const;
+    void operator+=(const Point &rhs);
+    Point operator+(const Point &rhs) const;
 
     int x, y;
 };
 
-template<const int ROWS, const int COLS>
-void DFS(const int(&mat)[ROWS][COLS], const int L, const int W, const Point current, int coin = 0);
+template <const int ROWS, const int COLS>
+void DFS(const int (&mat)[ROWS][COLS], const int L, const int W, const Point current, int coin = 0);
 
 int max_coin, check;
 
@@ -52,9 +52,9 @@ int main() {
 }
 // END MAIN
 
-template<const int ROWS, const int COLS>
-void DFS(const int(&mat)[ROWS][COLS], const int L, const int W, const Point current, int coin) {
-    static const Point d[3] = {Point(0,-1), Point(-1,-1), Point(1,-1)}; // Direction : up_straight, up_left, up_right.
+template <const int ROWS, const int COLS>
+void DFS(const int (&mat)[ROWS][COLS], const int L, const int W, const Point current, int coin) {
+    static const Point d[3] = {Point(0, -1), Point(-1, -1), Point(1, -1)}; // Direction : up_straight, up_left, up_right.
 
     if (current.y == 0 && max_coin < coin)
         max_coin = coin;
@@ -63,9 +63,9 @@ void DFS(const int(&mat)[ROWS][COLS], const int L, const int W, const Point curr
         Point next = current + d[i];
 
         if (next.x >= 0 && next.x < W && next.y >= 0) {
-            if (mat[next.y][next.x] != 2) {
+            if (mat[next.y][next.x] != 2)
                 DFS(mat, L, W, next, coin + mat[next.y][next.x]);
-            }
+
             else if (check) {
                 check = 0;
                 DFS(mat, L, W, next, coin);
@@ -85,26 +85,26 @@ Point::Point(int _x, int _y) {
     y = _y;
 }
 
-bool Point::operator==(const Point& rhs) const {
+bool Point::operator==(const Point &rhs) const {
     if (this->x == rhs.x && this->y == rhs.y)
         return true;
     else
         return false;
 }
 
-bool Point::operator!=(const Point& rhs) const {
+bool Point::operator!=(const Point &rhs) const {
     if (this->x == rhs.x && this->y == rhs.y)
         return false;
     else
         return true;
 }
 
-void Point::operator+=(const Point& rhs) {
+void Point::operator+=(const Point &rhs) {
     this->x += rhs.x;
     this->y += rhs.y;
 }
 
-Point Point::operator+(const Point& rhs) const {
+Point Point::operator+(const Point &rhs) const {
     Point tmp;
 
     tmp.x = this->x + rhs.x;
