929668fb5ad2698766eb391da39e3f58420eecab
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-13 13:06:05 +0700

    .

--- Game_with_numbers/main.cpp
+++ Game_with_numbers/main.cpp
@@ -4,52 +4,77 @@ using namespace std;
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-const int MAX_N = 4; // Should be 30
+const int N_MAX = 30;
 
-int MinHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j);
-int MaxHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j);
+int EasyLevel(const int (&nums)[N_MAX], int N);
+int HardLevel(const int (&nums)[N_MAX], int N);
+int HardLevel(const int (&nums)[N_MAX], int (&hard)[N_MAX][N_MAX], int N, int left, int right);
 
 int main(int argc, char **argv) {
     const char *input = (argc > 1) ? argv[1] : "input.txt";
     freopen(input, "r", stdin);
+
     int T;
     cin >> T;
-    for (int tc = 0; tc < T; tc++) {
+
+    for (int t = 0; t < T; t++) {
         int N;
         cin >> N;
-        int num[MAX_N] = {};
-        for (int i = 0; i < N; i++) {
-            cin >> num[i];
-        }
+        int nums[N_MAX] = {};
+        for (int i = 0; i < N; i++)
+            cin >> nums[i];
 
-        int hard[MAX_N][MAX_N] = {};
-
-        int minHard = MinHard(num, hard, 0, N - 1);
-        int maxHard = MaxHard(num, hard, 0, N - 1);
-
-        cout << "Case #" << tc + 1 << endl;
-        cout << minHard << " " << maxHard << endl;
+        cout << "Case #" << t + 1 << endl
+             << EasyLevel(nums, N) << " " << HardLevel(nums, N) << endl;
     }
+
     return 0;
 }
 
-int MinHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j) {
-    return 0;
+int EasyLevel(const int (&nums)[N_MAX], int N) {
+    int you = 0, com = 0;
+    bool your_turn = true;
+
+    int left = 0, right = N - 1;
+
+    while (left < right) {
+        if (your_turn) {
+            if (nums[left] >= nums[right]) {
+                you += nums[left++];
+            } else {
+                you += nums[right--];
+            }
+        } else {
+            if (nums[left] >= nums[right]) {
+                com += nums[right--];
+            } else {
+                com += nums[left++];
+            }
+        }
+        your_turn = !your_turn;
+    }
+
+    return you;
 }
 
-int MaxHard(const int (&num)[MAX_N], int (&hard)[MAX_N][MAX_N], int i, int j) {
-    if (i > j)
+int HardLevel(const int (&nums)[N_MAX], int N) {
+    int hard[N_MAX][N_MAX] = {};
+    return HardLevel(nums, hard, N, 0, N - 1);
+}
+
+int HardLevel(const int (&nums)[N_MAX], int (&hard)[N_MAX][N_MAX], int N, int left, int right) {
+    if (left > right)
         return 0;
 
-    if (hard[i][j])
-        return hard[i][j];
+    if (hard[left][right])
+        return hard[left][right];
 
-    int s1 = num[i] + MaxHard(num, hard, i + 1, j - 1);
-    int s2 = num[i] + MaxHard(num, hard, i + 2, j);
-    int s3 = num[j] + MaxHard(num, hard, i + 1, j - 1);
-    int s4 = num[j] + MaxHard(num, hard, i, j - 2);
+    int s1 = nums[left] + HardLevel(nums, hard, N, left + 1, right - 1);
+    int s2 = nums[left] + HardLevel(nums, hard, N, left + 2, right);
+    int s3 = nums[right] + HardLevel(nums, hard, N, left + 1, right - 1);
+    int s4 = nums[right] + HardLevel(nums, hard, N, left, right - 2);
 
-    hard[i][j] = MAX(MIN(s1, s2), MIN(s3, s4));
+    hard[left][right] = MAX(MIN(s1, s2), MIN(s3, s4));
 
-    return hard[i][j];
+    return hard[left][right];
 }
