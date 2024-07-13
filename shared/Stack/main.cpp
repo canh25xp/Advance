e4d698dd0d76a14b424f6d57669bb204b5308aaf
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-13 20:44:07 +0700

    Add Stack test

---
+++ shared/Stack/main.cpp
@@ -0,0 +1,18 @@
+#include "stack.hpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Stack<> s;
+
+    s.pop();
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    int a = s.pop();
+
+    return 0;
+}
