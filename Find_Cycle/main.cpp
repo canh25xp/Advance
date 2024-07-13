6d057b36af0148f4848caf9ae04a0ba005398786
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-13 20:34:16 +0700

    move stack implement to its own file

--- Find_Cycle/main.cpp
+++ Find_Cycle/main.cpp
@@ -1,109 +1,97 @@
-#define _CRT_SECURE_NO_WARNINGS
+#include "stack.hpp"
 #include <iostream>
 using namespace std;
 
 const int SIZE = 100;
+const int STACK_SIZE = 1000;
 
-template <typename T, unsigned int MAX>
-class Stack
-{
-public:
-    Stack();
-    ~Stack();
-
-    bool isFull();
-    bool isEmpty();
-    void push(T item);
-    T pop();
-
+class Graph {
 private:
-    T items[MAX];
-    int top;
-};
-
-class Graph{
-private:
-    int adj[SIZE][SIZE] ; // adjacent matrix
+    int adj[SIZE][SIZE]; // adjacent matrix
     int nodes, edges;
 
 public:
     Graph(int nodes, int edges);
 
     void add_edge(int u, int v);
-    bool DFS(int (&vst)[SIZE], int vertex, int target, int step=0);
+    bool DFS(int (&vst)[SIZE], int vertex, int target, int step = 0);
     bool DFS(int vertex, int target);
     bool hasPath(int start, int target);
     bool hasCycle();
 };
 
-int main(int argc, char* argv[]){
-    ios::sync_with_stdio(false);
-    const char* input = argv[1];
+int main(int argc, char *argv[]) {
+    const char *input = (argc > 1) ? argv[1] : "input.txt";
+    const char *log = "log.txt";
     freopen(input, "r", stdin);
+    freopen(log, "w", stderr);
 
-    int T; cin >> T;
+    int T;
+    cin >> T;
 
-    for (int tc = 0; tc < T; tc++){
+    for (int tc = 0; tc < T; tc++) {
         int nodes, edges;
         cin >> nodes >> edges;
 
         Graph g(nodes, edges);
 
-        for (int i = 0; i < edges; i++){
-            int u,v;
+        for (int i = 0; i < edges; i++) {
+            int u, v;
             cin >> u >> v;
-            g.add_edge(u-1, v-1);
+            g.add_edge(u - 1, v - 1);
         }
 
         bool hasCycles = g.hasCycle();
-        cout << "Case #" << tc+1 << "\n" << hasCycles << endl;
+        cout << "Case #" << tc + 1 << "\n"
+             << hasCycles << endl;
     }
 
     return 0;
 }
 
-Graph::Graph(int nodes, int edges){
+Graph::Graph(int nodes, int edges) {
     this->nodes = nodes;
     this->edges = edges;
-    for (int i = 0; i < nodes; i++){
-        for (int j = 0; j < nodes; j++){
+    for (int i = 0; i < nodes; i++) {
+        for (int j = 0; j < nodes; j++) {
             adj[i][j] = 0;
         }
     }
 }
 
-void Graph::add_edge(int u, int v){
-    adj[u][v] = 1;   // Set edge from u to v
-    //adj[v][u] = 1; // Set edge from v to u (if undirected graph)
+void Graph::add_edge(int u, int v) {
+    adj[u][v] = 1; // Set edge from u to v
+    // adj[v][u] = 1; // Set edge from v to u (if undirected graph)
 }
 
-bool Graph::DFS(int (&vst)[SIZE], int vertex, int target, int step){
+bool Graph::DFS(int (&vst)[SIZE], int vertex, int target, int step) {
     if (vertex == target && step > 0)
         return true;
 
-    if (step>0)
+    if (step > 0)
         vst[vertex] = 1;
 
     for (int i = 0; i < nodes; i++)
         if (adj[vertex][i] == 1 && !vst[i])
-            if (DFS(vst, i, target, step+1))
+            if (DFS(vst, i, target, step + 1))
                 return true;
 
     return false;
 }
 
-bool Graph::DFS(int vertex, int target){
+bool Graph::DFS(int vertex, int target) {
     int vst[SIZE] = {};
-    Stack<int, SIZE> s;
+    Stack<int, STACK_SIZE> s;
+
     s.push(vertex);
-    while (!s.isEmpty()){
+    while (!s.isEmpty()) {
         int v = s.pop();
         vst[v] = 1;
-        for (int i = 0; i < nodes; i++){
-            if (adj[vertex][i] == 1){
+        for (int i = 0; i < nodes; i++) {
+            if (adj[vertex][i] == 1) {
                 if (!vst[i])
                     s.push(i);
-                else if(i == target)
+                else if (i == target)
                     return true;
             }
         }
@@ -112,67 +100,21 @@ bool Graph::DFS(int vertex, int target){
     return false;
 }
 
-
-bool Graph::hasCycle(){
-    //for (int i = 0; i < nodes; i++)
-    //    if(hasPath(i, i))
-    //        return true;
-
-    //return false;
-
+bool Graph::hasCycle() {
     for (int i = 0; i < nodes; i++)
-        if(DFS(i, i))
+        if(hasPath(i, i))
             return true;
+
+    return false;
+
+    // for (int i = 0; i < nodes; i++)
+    //     if (DFS(i, i))
+    //         return true;
 
     return false;
 }
 
-bool Graph::hasPath(int start, int target){
+bool Graph::hasPath(int start, int target) {
     int vst[SIZE] = {};
     return DFS(vst, start, target);
-}
-
-template <typename T, unsigned int MAX>
-Stack<T, MAX>::Stack()
-{
-    this->top = -1;
-}
-
-template <typename T, unsigned int MAX>
-Stack<T, MAX>::~Stack()
-{
-}
-
-template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isEmpty(){
-    if (this->top == -1)
-        return true;
-    else
-        return false;
-}
-
-template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isFull(){
-    if (this->top == MAX - 1)
-        return true;
-    else
-        return false;
-}
-
-template <typename T, unsigned int MAX>
-void Stack<T, MAX>::push(T item){
-    if (this->isFull())
-        return;
-    this->top++;
-    this->items[this->top] = item;
-}
-
-template <typename T, unsigned int MAX>
-T Stack<T, MAX>::pop(){
-    T item;
-    if (isEmpty())
-        return item;
-    item = this->items[this->top];
-    this->top--;
-    return item;
 }
---
+++ shared/Stack/stack.hpp
@@ -0,0 +1,56 @@
+template <typename T, int MAX>
+class Stack {
+private:
+    int top;
+    T items[MAX];
+
+public:
+    Stack();
+    ~Stack();
+
+    void push(T item);
+    T pop();
+    bool isFull();
+    bool isEmpty();
+};
+
+template <typename T, int MAX>
+Stack<T, MAX>::Stack() : top(-1) {}
+
+template <typename T, int MAX>
+Stack<T, MAX>::~Stack() {}
+
+template <typename T, int MAX>
+void Stack<T, MAX>::push(T item) {
+    if (this->isFull()) {
+        std::cerr << "Stack Overflow \n";
+        return;
+    }
+    this->top++;
+    this->items[this->top] = item;
+}
+
+template <typename T, int MAX>
+T Stack<T, MAX>::pop() {
+    T item{};
+    if (isEmpty()) {
+        std::cerr << "Stack Underflow \n";
+        return item;
+    }
+    item = this->items[this->top];
+    this->top--;
+    return item;
+}
+
+template <typename T, int MAX>
+bool Stack<T, MAX>::isEmpty() {
+    return (this->top < 0);
+}
+
+template <typename T, int MAX>
+bool Stack<T, MAX>::isFull() {
+    if (this->top >= (MAX - 1))
+        return true;
+    else
+        return false;
+}
