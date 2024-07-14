b6353668ec097117b1a45ac9aa3caecbd3e346d3
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-14 17:37:22 +0700

    re-implement queue

--- shared/Queue/queue.hpp
+++ shared/Queue/queue.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-template <typename T = int, unsigned int MAX = 10000>
+template <typename T = int, int MAX = 10000>
 class Queue {
 private:
     int front, rear, count;
@@ -21,76 +21,76 @@ public:
     void clear();
 };
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 Queue<T, MAX>::Queue() : front(-1), rear(-1), count(0) {}
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 Queue<T, MAX>::~Queue() {}
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 bool Queue<T, MAX>::isEmpty() const {
-    if (front == rear)
-        return true;
-    else
-        return false;
+    return front == rear;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 bool Queue<T, MAX>::isFull() const {
-    if (front == 0 && rear == MAX - 1)
-        return true;
-    else
-        return false;
+    return front == 0 && rear == MAX - 1;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 void Queue<T, MAX>::enQueue(const T &item) {
-    if (isFull())
+    if (isFull()) {
+        std::cerr << "Queue is full";
         return;
+    }
     rear++;
     count++;
     items[rear] = item;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 T Queue<T, MAX>::deQueue() {
-    T item;
-    if (isEmpty())
+    T item{};
+    if (isEmpty()) {
+        std::cerr << "Queue is empty";
         return item;
+    }
 
     front++;
     count--;
-    item = this->items[front];
+    item = items[front];
     return item;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 T Queue<T, MAX>::peek() const {
-    T item;
-    if (isEmpty())
+    T item{};
+    if (isEmpty()) {
+        std::cerr << "Queue is empty";
         return item;
-    item = this->items[front];
+    }
+    item = items[front];
     return item;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 int Queue<T, MAX>::size() const {
     return count;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 bool Queue<T, MAX>::has(const T &item) const {
     for (int i = 0; i < this->size(); ++i) {
-        if (this->items[(this->front + i) % MAX] == item) {
+        if (items[(front + i) % MAX] == item) {
             return true;
         }
     }
     return false;
 }
 
-template <typename T, unsigned int MAX>
+template <typename T, int MAX>
 void Queue<T, MAX>::clear() {
-    this->front = -1;
-    this->rear = -1;
-    this->count = 0;
+    front = -1;
+    rear = -1;
+    count = 0;
 }
