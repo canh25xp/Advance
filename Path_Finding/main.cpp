fd45b189820466910774f0524195a339ac74f093
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-14 16:21:33 +0700

    rename var and stuff :v

--- Path_Finding/main.cpp
+++ Path_Finding/main.cpp
@@ -1,26 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "stack.hpp"
 #include "point.hpp"
+#include "stack.hpp"
 #include <iostream>
 
 using namespace std;
 
-#define MATRIX_SIZE 21
-#define SIZE 100000
+const int N_MAX = 21;
+const int STACK_SIZE = 100000;
 
-int TEST_CASE = 0;
-int N = 0;
-
-bool solveMaze(Point start_point, Point end_point, int matrix[MATRIX_SIZE][MATRIX_SIZE]);
+bool solve(Point start_point, Point end_point, int matrix[N_MAX][N_MAX], const int &N);
 
 int main(int argc, char **argv) {
     const char *input = (argc > 1) ? argv[1] : "input.txt";
     freopen(input, "r", stdin);
 
-    cin >> TEST_CASE;
-    for (int case_index = 0; case_index < TEST_CASE; case_index++) {
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
+        int N;
         cin >> N;
-        int matrix[MATRIX_SIZE][MATRIX_SIZE] = {};
+        int matrix[N_MAX][N_MAX] = {};
         for (int row = 0; row < N; row++) {
             for (int col = 0; col < N; col++) {
                 cin >> matrix[row][col];
@@ -30,7 +29,7 @@ int main(int argc, char **argv) {
         Point start_point(0, 0);
         Point end_point(N - 1, N - 1);
 
-        bool flag = solveMaze(start_point, end_point, matrix);
+        bool flag = solve(start_point, end_point, matrix, N);
 
         if (flag == 1)
             cout << "YES";
@@ -42,13 +41,13 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-bool solveMaze(Point start_point, Point end_point, int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+bool solve(Point start_point, Point end_point, int matrix[N_MAX][N_MAX], const int &N) {
     // up, right, down, left
     const int dx[] = {0, 1, 0, -1};
     const int dy[] = {-1, 0, 1, 0};
-    Stack<Point> stack;
+    Stack<Point, STACK_SIZE> stack;
     stack.push(start_point);
-    bool checked[MATRIX_SIZE][MATRIX_SIZE] = {};
+    bool checked[N_MAX][N_MAX] = {};
 
     while (!stack.isEmpty()) {
         Point current_point = stack.pop();
