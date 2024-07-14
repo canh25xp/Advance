b42951282050990c5c94cf508e548e5b9f964490
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-14 18:01:29 +0700

    move graph implement to its own file

--- Route_Finding/main.cpp
+++ Route_Finding/main.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include "graph.hpp"
 #include <iostream>
 
 using namespace std;
@@ -6,21 +7,8 @@ using namespace std;
 const int T = 10;
 const int MAX_NODES = 100;
 
-class Graph {
-private:
-    int adj[MAX_NODES][MAX_NODES]; // adjacent matrix
-    int size;
-
-public:
-    Graph(int n);
-
-    void add_edge(int u, int v);
-    bool DFS(int vertex, int vst[], int target);
-    bool hasPath(int start, int target);
-};
-
 int main(int argc, char *argv[]) {
-    const char *input = argv[1];
+    const char *input = (argc > 1) ? argv[1] : "input.txt";
     freopen(input, "r", stdin);
 
     for (int t = 0; t < T; t++) {
@@ -29,7 +17,7 @@ int main(int argc, char *argv[]) {
         int N;
         cin >> N;
 
-        Graph g(MAX_NODES);
+        Graph<int, MAX_NODES> g(MAX_NODES);
 
         for (int i = 0; i < N; i++) {
             int u, v;
@@ -37,45 +25,10 @@ int main(int argc, char *argv[]) {
             g.add_edge(u, v);
         }
 
-        bool found = g.hasPath(0, 99);
+        bool found = g.DFS(0, 99);
 
         cout << "#" << t + 1 << " " << found << endl;
     }
 
     return 0;
-}
-
-Graph::Graph(int n) {
-    size = n;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            adj[i][j] = 0;
-        }
-    }
-}
-
-void Graph::add_edge(int u, int v) {
-    adj[u][v] = 1; // Set edge from u to v
-    // adj[v][u] = 1; // Set edge from v to u (if undirected graph)
-}
-
-bool Graph::DFS(int vertex, int vst[], int target) {
-    vst[vertex] = 1;
-
-    // cout << "(" << u << "," << v << ")" << endl;
-
-    if (vertex == target)
-        return true;
-
-    for (int i = 0; i < size; i++)
-        if (adj[vertex][i] == 1 && !vst[i])
-            if (DFS(i, vst, target))
-                return true;
-
-    return false;
-}
-
-bool Graph::hasPath(int start, int target) {
-    int vst[MAX_NODES] = {};
-    return DFS(start, vst, target);
 }
---
+++ shared/Graph/graph.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+
+template <typename T = int, int MAX = 10000>
+class Graph {
+private:
+    int size;
+    int adj[MAX][MAX]; // adjacent matrix
+
+public:
+    Graph(int n);
+    T &operator()(int i, int j);
+    T operator()(int i, int j) const;
+
+    void add_edge(int u, int v);
+    bool DFS(int vertex, int target, int vst[]);
+    bool DFS(int start, int target);
+};
+
+template <typename T, int MAX>
+Graph<T, MAX>::Graph(int n) {
+    size = n;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            adj[i][j] = 0;
+}
+
+template <typename T, int MAX>
+T &Graph<T, MAX>::operator()(int i, int j) {
+    return adj[i][j];
+}
+
+template <typename T, int MAX>
+T Graph<T, MAX>::operator()(int i, int j) const {
+    return adj[i][j];
+}
+
+template <typename T, int MAX>
+void Graph<T, MAX>::add_edge(int u, int v) {
+    adj[u][v] = 1; // Set edge from u to v
+    // adj[v][u] = 1; // Set edge from v to u (if undirected graph)
+}
+
+template <typename T, int MAX>
+bool Graph<T, MAX>::DFS(int vertex, int target, int vst[]) {
+    vst[vertex] = 1;
+
+    if (vertex == target)
+        return true;
+
+    for (int i = 0; i < size; i++)
+        if (adj[vertex][i] == 1 && !vst[i])
+            if (DFS(i, target, vst))
+                return true;
+
+    return false;
+}
+
+template <typename T, int MAX>
+bool Graph<T, MAX>::DFS(int start, int target) {
+    int vst[MAX_NODES] = {};
+    return DFS(start, target, vst);
+}
