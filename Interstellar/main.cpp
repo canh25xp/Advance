d7182d53f07aec9cbfe72cecf3b4792758e60474
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-16 21:25:39 +0700

    i fucked up

--- Interstellar/main.cpp
+++ Interstellar/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 
+const int SIZE = 5;
+
+struct Point {
+    int x, y;
+
+    Point() : x(0), y(0) {}
+
+    Point(int x, int y) : x(x), y(y) {}
+};
+
 using namespace std;
 int main(int argc, char **argv) {
-    
-    freopen();
+    const char *input = (argc > 1) ? argv[1] : "input.txt";
+    freopen(input, "r", stdin);
+
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < 0; t++) {
+        int N;
+        cin >> N;
+
+        Point S, D;
+
+        cin >> S.x >> S.y >> D.x >> D.y;
+
+        Point A[SIZE], B[SIZE];
+        int time[SIZE];
+        for (int i = 0; i < N; i++)
+            cin >> A[i].x >> A[i].y >> B[i].x >> B[i].y >> time[i];
+
+        cout << "#" << t + 1 << endl;
+    }
 
     return 0;
 }
