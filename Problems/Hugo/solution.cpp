671ecd566da3e218dcb68ef88eec3d4bc5cf194e
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-19 21:20:54 +0700

    rename var

--- Problems/Hugo/solution.cpp
+++ Problems/Hugo/solution.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int t, tc, n, m, sx, sy, p, thoat, ans, front, rear;
-int a[20][20], kc[20][20], qx[600], qy[600], dchay[20][20], visited[20][20], lk[20][20];
+int lakes[20][20], diamonds[20][20], qx[600], qy[600], fires[20][20], visited[20][20], gates[20][20];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -46,7 +46,7 @@ void chayLan() {
     int i, j, x, y, tx, ty, k;
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= m; j++) {
-            if (dchay[i][j] == 0) {
+            if (fires[i][j] == 0) {
                 enQueue(i, j);
                 // dchay[i][j] = 1;
             }
@@ -58,9 +58,9 @@ void chayLan() {
         for (k = 0; k < 4; k++) {
             tx = x + dx[k];
             ty = y + dy[k];
-            if (dk(tx, ty) && (dchay[tx][ty] == 5000 || dchay[tx][ty] > dchay[x][y] + 1) && a[tx][ty] != 1) {
+            if (dk(tx, ty) && (fires[tx][ty] == 5000 || fires[tx][ty] > fires[x][y] + 1) && lakes[tx][ty] != 1) {
                 enQueue(tx, ty);
-                dchay[tx][ty] = dchay[x][y] + 1;
+                fires[tx][ty] = fires[x][y] + 1;
             }
         }
     }
@@ -70,11 +70,11 @@ void DFS(int i, int j, int time, int score) {
     int h = time;
     int c = score;
 
-    if (lk[i][j] == 3) {
+    if (gates[i][j] == 3) {
         if (c > ans)
             ans = c;
     }
-    if (a[i][j] == 1)
+    if (lakes[i][j] == 1)
         h = h + 2;
     else
         h = h + 1;
@@ -82,9 +82,9 @@ void DFS(int i, int j, int time, int score) {
     for (int k = 0; k < 4; k++) {
         int tx = i + dx[k];
         int ty = j + dy[k];
-        if (dk(tx, ty) && visited[tx][ty] == 0 && (a[tx][ty] == 1 || h < dchay[tx][ty])) {
+        if (dk(tx, ty) && visited[tx][ty] == 0 && (lakes[tx][ty] == 1 || h < fires[tx][ty])) {
             visited[tx][ty] = 1;
-            DFS(tx, ty, h, c + kc[tx][ty]);
+            DFS(tx, ty, h, c + diamonds[tx][ty]);
             visited[tx][ty] = 0;
         }
     }
@@ -100,39 +100,38 @@ int main() {
         int i, j, x, h, c;
         for (i = 1; i <= n; i++) {
             for (j = 1; j <= m; j++) {
-                a[i][j] = 0;
-                dchay[i][j] = 5000;
+                lakes[i][j] = 0;
+                fires[i][j] = 5000;
                 visited[i][j] = 0;
-                lk[i][j] = 0;
+                gates[i][j] = 0;
             }
         }
         cin >> p;
         for (x = 0; x < p; x++) {
             cin >> h >> c;
-            // a[h][c] = 2;
-            dchay[h][c] = 0;
+            fires[h][c] = 0;
         }
         cin >> p;
         for (x = 0; x < p; x++) {
             cin >> h >> c;
-            a[h][c] = 1;
+            lakes[h][c] = 1;
         }
         cin >> thoat;
         for (x = 0; x < thoat; x++) {
             cin >> h >> c;
-            lk[h][c] = 3;
+            gates[h][c] = 3;
         }
         for (i = 1; i <= n; i++) {
             for (j = 1; j <= m; j++) {
-                cin >> kc[i][j];
+                cin >> diamonds[i][j];
             }
         }
 
         chayLan();
         ans = -1;
         visited[sx][sy] = 1;
-        if (a[sx][sy] != 2)
-            DFS(sx, sy, 0, kc[sx][sy]);
+        if (lakes[sx][sy] != 2)
+            DFS(sx, sy, 0, diamonds[sx][sy]);
         cout << "Case #" << tc << endl;
         cout << ans << endl;
     }
