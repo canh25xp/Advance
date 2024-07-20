928b5046d0ed7411ebda7efc920658929af778a6
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-20 19:04:41 +0700

    format code

--- Problems/Move_Cow/main.cpp
+++ Problems/Move_Cow/main.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 const int N_MAX = 16;
@@ -11,51 +11,48 @@ int find_max(int (&arr)[SIZE], int n, int limit);
 
 int find_max_capacity(int (&arr)[N_MAX], int n, int limit);
 
-int main(int argc, char** argv){
-	ios::sync_with_stdio(false);
-	freopen("input.txt", "r", stdin);
+int main(int argc, char **argv) {
+    const char *input = (argc > 1) ? argv[1] : "input.txt";
+    freopen(input, "r", stdin);
 
-	int TEST_CASE;
-	cin >> TEST_CASE;
+    int T;
+    cin >> T;
 
-	for(int test_case = 1; test_case <= TEST_CASE; ++test_case){
-		int ans = 0;
-		int loading_capacity, cows;
-		cin >> loading_capacity >> cows;
+    for (int ti = 1; ti <= T; ++ti) {
+        int loading_capacity, cows;
+        cin >> loading_capacity >> cows;
 
-		int weights[N_MAX] = {};
-        
+        int weights[N_MAX] = {};
+
         for (int i = 0; i < cows; i++)
             cin >> weights[i];
 
-		ans = find_max_capacity(weights, cows, loading_capacity);
-
-		cout << "#" << test_case << " " << ans << endl;
-	}
-	return 0;
+        cout << "#" << ti << " " << find_max_capacity(weights, cows, loading_capacity) << endl;
+    }
+    return 0;
 }
 
 int find_max_capacity(int (&arr)[N_MAX], int n, int limit) {
     int cummcumulative_sum[SIZE] = {};
-    
-    int index = 0;
-	for(int i = 0; i < n; i++) {
-		cummcumulative_sum[index] = arr[i];
-		index++;
-		for(int j = index - 2; j >= 0; j--){
-			cummcumulative_sum[index] = arr[i] + cummcumulative_sum[j];
-			index++;
-		}
-	}
 
-	return find_max(cummcumulative_sum, index, limit);
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+        cummcumulative_sum[index] = arr[i];
+        index++;
+        for (int j = index - 2; j >= 0; j--) {
+            cummcumulative_sum[index] = arr[i] + cummcumulative_sum[j];
+            index++;
+        }
+    }
+
+    return find_max(cummcumulative_sum, index, limit);
 }
 
 int find_max(int (&arr)[SIZE], int n, int limit) {
-	int max = 0;
-	for(int i = 0; i < n; i++)
-		if(arr[i] > max && arr[i] <= limit)
-			max = arr[i];
-	
-	return max;
+    int max = 0;
+    for (int i = 0; i < n; i++)
+        if (arr[i] > max && arr[i] <= limit)
+            max = arr[i];
+
+    return max;
 }
