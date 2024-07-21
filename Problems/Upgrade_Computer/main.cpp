287ecdc1e4e44e2a16102c80f400155e5ccc3caa
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-21 21:29:31 +0700

    nah I'm out.

--- Problems/Upgrade_Computer/main.cpp
+++ Problems/Upgrade_Computer/main.cpp
@@ -6,7 +6,7 @@ const int M = 30;
 int n, m, required, ans;
 int original_price[50], discount_price[50], discount_part[50][30], required_part[50], bought[50];
 
-void backtrack(int x, int tien);
+void backtrack(int, int);
 
 using namespace std;
 int main(int argc, char **argv) {
@@ -14,23 +14,23 @@ int main(int argc, char **argv) {
     freopen(input, "r", stdin);
     int T;
     cin >> T;
-    for (int tc = 1; tc <= T; tc++) {
+    for (int tc = 0; tc = T; tc++) {
         cin >> n;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             cin >> original_price[i];
             bought[i] = 0;
         }
         cin >> m;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 discount_part[i][j] = 0;
             }
         }
-        for (int i = 1; i <= m; i++) {
+        for (int i = 0; i < m; i++) {
             cin >> discount_price[i];
             int x;
             cin >> x;
-            for (int j = 1; j <= x; j++) {
+            for (int j = 0; j < x; j++) {
                 int l;
                 cin >> l;
                 discount_part[i][l] = 1;
@@ -38,13 +38,13 @@ int main(int argc, char **argv) {
         }
 
         cin >> required;
-        for (int i = 1; i <= required; i++)
+        for (int i = 0; i < required; i++)
             cin >> required_part[i];
 
         ans = INT_MAX;
-        backtrack(1, 0);
+        backtrack(0, 0);
 
-        cout << "#" << tc << " " << ans << endl;
+        cout << "#" << tc + 1 << " " << ans << endl;
     }
     return 0;
 }
@@ -63,9 +63,9 @@ void backtrack(int index, int total_price) {
                 backtrack(index + 1, total_price + original_price[tb]);
                 bought[tb]--;
             } else {
-                for (int k = 1; k <= m; k++) {
+                for (int k = 0; k < m; k++) {
                     if (discount_part[k][tb]) {
-                        for (int i = 0; i <= n; i++) {
+                        for (int i = 0; i < n; i++) {
                             if (discount_part[k][i])
                                 bought[i]++;
                         }
