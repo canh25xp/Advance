62faa82d1e03515b2be96a857a70d9d70902da06
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-21 23:25:23 +0700

    bruh

--- Problems/Bipartitioning_Cities/main.cpp
+++ Problems/Bipartitioning_Cities/main.cpp
@@ -6,9 +6,9 @@ const int N = 1000;
 
 int adj[N][N] = {};
 
-// This function returns true if graph
-// G[N][N] is Bipartite, else false
-bool isBipartite(int G[N][N], int V, int src = 0);
+bool isBipartiteUtil(int (&G)[N][N], int V, int src, int (&colorArr)[N]);
+
+bool isBipartite(int (&G)[N][N], int V);
 
 using namespace std;
 int main() {
@@ -20,6 +20,8 @@ int main() {
         for (int i = 0; i < edges; i++) {
             int u, v;
             cin >> u >> v;
+            u--;
+            v--;
             adj[u][v] = 1;
             adj[v][u] = 1;
         }
@@ -31,33 +33,31 @@ int main() {
     return 0;
 }
 
-bool isBipartite(int G[N][N], int V, int src) {
-    // Create a color array to store colors
-    // assigned to all vertices. Vertex
-    // number is used as index in this array.
-    // The value '-1' of colorArr[i]
-    // is used to indicate that no color
-    // is assigned to vertex 'i'. The value 1
-    // is used to indicate first color
-    // is assigned and value 0 indicates
-    // second color is assigned.
+bool isBipartite(int (&G)[N][N], int V) {
+    // Create a color array to store colors assigned to all vertices.
+    // Vertex/number is used as index in this array.
+    // The value '-1' of colorArr[i] is used to indicate that no color is assigned to vertex 'i'.
+    // The value 1 is used to indicate first color is assigned,
+    // and value 0 indicates second color is assigned.
     int colorArr[N];
     for (int i = 0; i < V; ++i)
         colorArr[i] = -1;
 
-    // Assign first color to source
-    colorArr[src] = 1;
+    // This code is to handle disconnected graph
+    for (int i = 0; i < V; i++)
+        if (colorArr[i] == -1)
+            if (!isBipartiteUtil(G, V, i, colorArr))
+                return false;
 
-    // Create a queue (FIFO) of vertex
-    // numbers and enqueue source vertex
-    // for BFS traversal
+    return true;
+}
+
+bool isBipartiteUtil(int (&G)[N][N], int V, int src, int (&colorArr)[N]) {
     queue<int> q;
     q.push(src);
+    colorArr[src] = 1;
 
-    // Run while there are vertices
-    // in queue (Similar to BFS)
     while (!q.empty()) {
-        // Dequeue a vertex from queue ( Refer http://goo.gl/35oz8 )
         int u = q.front();
         q.pop();
 
@@ -70,7 +70,8 @@ bool isBipartite(int G[N][N], int V, int src) {
             // An edge from u to v exists and
             // destination v is not colored
             if (G[u][v] && colorArr[v] == -1) {
-                // Assign alternate color to this adjacent v of u
+                // Assign alternate color to this
+                // adjacent v of u
                 colorArr[v] = 1 - colorArr[u];
                 q.push(v);
             }
@@ -82,7 +83,7 @@ bool isBipartite(int G[N][N], int V, int src) {
         }
     }
 
-    // If we reach here, then all adjacent
-    // vertices can be colored with alternate color
+    // If we reach here, then all adjacent vertices can
+    // be colored with alternate color
     return true;
 }
