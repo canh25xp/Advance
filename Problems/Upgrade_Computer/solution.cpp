e4f89be50d3d5f468bb51ca1b385dcd4969e0393
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-21 21:17:59 +0700

    .

--- Problems/Upgrade_Computer/solution.cpp
+++ Problems/Upgrade_Computer/solution.cpp
@@ -1,75 +1,75 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int t, tc, n, m, l, ans;
-int price[50], discount_price[50], discount_part[50][30], required_part[50], bought[50];
+int t, tc, n, m, cm, ans;
+int gg[50], gm[50], lk[50][30], tbc[50], dm[50];
 
-void backtrack(int x, int tien){
-    if (x == l + 1) {
+void backtrack(int x, int tien) {
+    if (x == cm + 1) {
         if (ans > tien)
             ans = tien;
         return;
     }
-        int tb = required_part[x];
-        if (bought[tb] == 0) {
-            for (int j = 0; j <= 1; j++) {
-                if (j == 0) {
-                    bought[tb]++;
-                    backtrack(x + 1, tien + price[tb]);
-                    bought[tb]--;
-                } else {
-                    for (int k = 1; k <= m; k++) {
-                        if (discount_part[k][tb]) {
-                            for (int i = 0; i <= n; i++) {
-                                if (discount_part[k][i])
-                                    bought[i]++;
-                            }
-                            backtrack(x + 1, tien + discount_price[k]);
-                            for (int i = 1; i <= n; i++) {
-                                if (discount_part[k][i])
-                                    bought[i]--;
-                            }
+    int tb = tbc[x];
+    if (dm[tb] == 0) {
+        for (int j = 0; j <= 1; j++) {
+            if (j == 0) {
+                dm[tb]++;
+                backtrack(x + 1, tien + gg[tb]);
+                dm[tb]--;
+            } else {
+                for (int k = 1; k <= m; k++) {
+                    if (lk[k][tb]) {
+                        for (int i = 0; i <= n; i++) {
+                            if (lk[k][i])
+                                dm[i]++;
+                        }
+                        backtrack(x + 1, tien + gm[k]);
+                        for (int i = 1; i <= n; i++) {
+                            if (lk[k][i])
+                                dm[i]--;
                         }
                     }
                 }
             }
-        } else {
-            backtrack(x + 1, tien);
         }
+    } else {
+        backtrack(x + 1, tien);
+    }
 }
 
 int main() {
-	freopen("input.txt", "r", stdin);
-	cin >> t;
-	for (tc = 1; tc <= t; tc++) {
-		cin >> n;
-		int i, j, x, l;
-		for (i = 1; i <= n; i++) {
-                    cin >> price[i];
-                    bought[i] = 0;
-                }
-                cin >> m;
-                for (i = 1; i <= m; i++) {
-                    for (j = 1; j <= n; j++) {
-                        discount_part[i][j] = 0;
-                    }
-                }
-                for (i = 1; i <= m; i++) {
-                    cin >> discount_price[i];
-                    cin >> x;
-                    for (j = 1; j <= x; j++) {
-                        cin >> l;
-                        discount_part[i][l] = 1;
-                    }
-                }
+    freopen("input.txt", "r", stdin);
+    cin >> t;
+    for (tc = 1; tc <= t; tc++) {
+        cin >> n;
+        int i, j, x, l;
+        for (i = 1; i <= n; i++) {
+            cin >> gg[i];
+            dm[i] = 0;
+        }
+        cin >> m;
+        for (i = 1; i <= m; i++) {
+            for (j = 1; j <= n; j++) {
+                lk[i][j] = 0;
+            }
+        }
+        for (i = 1; i <= m; i++) {
+            cin >> gm[i];
+            cin >> x;
+            for (j = 1; j <= x; j++) {
                 cin >> l;
-                for (i = 1; i <= l; i++)
-                    cin >> required_part[i];
-                ans = 1000000002;
-                backtrack(1, 0);
+                lk[i][l] = 1;
+            }
+        }
+        cin >> cm;
+        for (i = 1; i <= cm; i++)
+            cin >> tbc[i];
+        ans = 1000000002;
+        backtrack(1, 0);
 
-                cout << "#" << tc << " " << ans << endl;
-	}
-	return 0;
+        cout << "#" << tc << " " << ans << endl;
+    }
+    return 0;
 }
