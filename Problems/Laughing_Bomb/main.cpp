c4ff16f6512a3472f4e7d9c8ecc1b0fc5cd05400
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-23 01:31:58 +0700

    nah we good

--- Problems/Laughing_Bomb/main.cpp
+++ Problems/Laughing_Bomb/main.cpp
@@ -9,8 +9,8 @@ const int MAX_N = 100; // should be 100
 const int MAX_M = 100; // should be 100
 
 // Direction : right down, left, up
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int di[4] = {-1, 0, 1, 0};
+int dj[4] = {0, 1, 0, -1};
 
 int main() {
     freopen("input.txt", "r", stdin);
@@ -20,31 +20,29 @@ int main() {
     cin >> T;
     for (int tc = 0; tc < T; tc++) {
         int N, M;
-        cin >> N >> M;
+        cin >> M >> N;
 
         int mat[MAX_M][MAX_N];
-        for (int m = 0; m < M; m++) {
-            for (int n = 0; n < N; n++) {
-                cin >> mat[m][n];
-            }
-        }
-        Point bomb;
-        cin >> bomb.i >> bomb.j;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < M; j++)
+                cin >> mat[i][j];
 
+        Point bomb;
+        cin >> bomb.j >> bomb.i;
         bomb--;
 
-        int visited[MAX_M][MAX_N] = {};
+        int visited[MAX_N][MAX_M] = {};
         Queue<Point, 50000> q;
         int ans = 0;
-        visited[bomb.j][bomb.i] = 1;
+        visited[bomb.i][bomb.j] = 1;
         q.push(bomb);
         while (!q.empty()) {
             Point curr = q.pop();
             for (int d = 0; d < 4; d++) {
-                Point next(curr.i + dx[d], curr.j + dy[d]);
-                if (next.valid(N, M) && !visited[next.j][next.i] && mat[next.j][next.i]) {
-                    visited[next.j][next.i] = visited[curr.j][curr.i] + 1;
-                    ans = visited[next.j][next.i];
+                Point next(curr.i + di[d], curr.j + dj[d]);
+                if (next.valid(N, M) && !visited[next.i][next.j] && mat[next.i][next.j]) {
+                    visited[next.i][next.j] = visited[curr.i][curr.j] + 1;
+                    ans = visited[next.i][next.j];
                     q.push(next);
                 }
             }
