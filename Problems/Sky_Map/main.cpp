f9fec4df303796ced4a88a87e35b689f3a8fe2a2
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-22 23:07:28 +0700

    cleaning up

--- Problems/Sky_Map/main.cpp
+++ Problems/Sky_Map/main.cpp
@@ -6,12 +6,11 @@ using namespace std;
 const int MAX_SIZE = 25;
 
 // Direction : left, right, up, down
-const int dx[4] = {1,-1,0,0};
-const int dy[4] = {0,0,-1,1};
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
 
 template <typename T, unsigned int MAX>
-class Stack
-{
+class Stack {
 public:
     Stack();
     ~Stack();
@@ -26,86 +25,76 @@ private:
     int top;
 };
 
-struct Point
-{
+struct Point {
     Point();
     Point(int _x, int _y);
-    int x,y;
+    int x, y;
 };
 
-int main(){
-	FILE *input;
-	errno_t error = freopen_s(&input, "eval_input.txt", "r", stdin);
+int main() {
+    FILE *input;
+    errno_t error = freopen_s(&input, "eval_input.txt", "r", stdin);
 
-	int TEST_CASE = DEFAULT_TEST_CASE;
-	cin >> TEST_CASE;
-	for(int case_index = 0 ; case_index <TEST_CASE ; case_index++){
+    int TEST_CASE = DEFAULT_TEST_CASE;
+    cin >> TEST_CASE;
+    for (int case_index = 0; case_index < TEST_CASE; case_index++) {
         int size = 0;
         cin >> size;
 
         int matrix[MAX_SIZE][MAX_SIZE] = {};
 
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 cin >> matrix[i][j];
             }
         }
 
-		Stack<Point ,MAX_SIZE*MAX_SIZE> s;
+        Stack<Point, MAX_SIZE * MAX_SIZE> s;
         int checked[MAX_SIZE][MAX_SIZE] = {};
         int constellars = 0;
         int max = 0;
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                if (matrix[i][j] == 1 && checked[i][j] != 1)
-                {
-                    s.push(Point(j,i));
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] == 1 && checked[i][j] != 1) {
+                    s.push(Point(j, i));
                     checked[i][j] = 1;
                     constellars++;
                     int stars = 1;
-                    while (!s.isEmpty())
-                    {
+                    while (!s.isEmpty()) {
                         Point p = s.pop();
-                        for (int d = 0; d < 4; d++)
-                        {
+                        for (int d = 0; d < 4; d++) {
                             int x = p.x + dx[d];
                             int y = p.y + dy[d];
-                            if (matrix[y][x] == 1 && checked[y][x] != 1 && y >= 0 && y < size && x >= 0 && x < size){
-                                s.push(Point(x,y));
+                            if (matrix[y][x] == 1 && checked[y][x] != 1 && y >= 0 && y < size && x >= 0 && x < size) {
+                                s.push(Point(x, y));
                                 stars++;
                                 checked[y][x] = 1;
                             }
                         }
                     }
-                    if(stars>max)
+                    if (stars > max)
                         max = stars;
                 }
             }
         }
 
-		cout<< constellars << " " << max << endl;
-	}
+        cout << constellars << " " << max << endl;
+    }
 
-	return 0;
+    return 0;
 }
 
 template <typename T, unsigned int MAX>
-Stack<T, MAX>::Stack()
-{
+Stack<T, MAX>::Stack() {
     this->top = -1;
 }
 
 template <typename T, unsigned int MAX>
-Stack<T, MAX>::~Stack()
-{
+Stack<T, MAX>::~Stack() {
 }
 
 template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isEmpty(){
+bool Stack<T, MAX>::isEmpty() {
     if (this->top == -1)
         return true;
     else
@@ -113,7 +102,7 @@ bool Stack<T, MAX>::isEmpty(){
 }
 
 template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isFull(){
+bool Stack<T, MAX>::isFull() {
     if (this->top == MAX - 1)
         return true;
     else
@@ -121,7 +110,7 @@ bool Stack<T, MAX>::isFull(){
 }
 
 template <typename T, unsigned int MAX>
-void Stack<T, MAX>::push(T item){
+void Stack<T, MAX>::push(T item) {
     if (this->isFull())
         return;
     this->top++;
@@ -129,7 +118,7 @@ void Stack<T, MAX>::push(T item){
 }
 
 template <typename T, unsigned int MAX>
-T Stack<T, MAX>::pop(){
+T Stack<T, MAX>::pop() {
     T item;
     if (isEmpty())
         return item;
@@ -138,12 +127,12 @@ T Stack<T, MAX>::pop(){
     return item;
 }
 
-Point::Point(){
+Point::Point() {
     this->x = 0;
     this->y = 0;
 }
 
-Point::Point(int _x, int _y){
+Point::Point(int _x, int _y) {
     this->x = _x;
     this->y = _y;
 }
