f858f674b9e322dfcbafc722a55624b233615689
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-22 23:16:36 +0700

    refactor

--- Problems/Sky_Map/main.cpp
+++ Problems/Sky_Map/main.cpp
@@ -1,53 +1,27 @@
+#include "point.hpp"
+#include "stack.hpp"
 #include <iostream>
-using namespace std;
-
-#define DEFAULT_TEST_CASE 10
 
 const int MAX_SIZE = 25;
 
-// Direction : left, right, up, down
-const int dx[4] = {1, -1, 0, 0};
-const int dy[4] = {0, 0, -1, 1};
+const int di[4] = {0, 0, -1, 1};
+const int dj[4] = {1, -1, 0, 0};
 
-template <typename T, unsigned int MAX>
-class Stack {
-public:
-    Stack();
-    ~Stack();
-
-    bool isFull();
-    bool isEmpty();
-    void push(T item);
-    T pop();
-
-private:
-    T items[MAX];
-    int top;
-};
-
-struct Point {
-    Point();
-    Point(int _x, int _y);
-    int x, y;
-};
-
+using namespace std;
 int main() {
-    FILE *input;
-    errno_t error = freopen_s(&input, "eval_input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
 
-    int TEST_CASE = DEFAULT_TEST_CASE;
-    cin >> TEST_CASE;
-    for (int case_index = 0; case_index < TEST_CASE; case_index++) {
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++) {
         int size = 0;
         cin >> size;
 
         int matrix[MAX_SIZE][MAX_SIZE] = {};
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
                 cin >> matrix[i][j];
-            }
-        }
 
         Stack<Point, MAX_SIZE * MAX_SIZE> s;
         int checked[MAX_SIZE][MAX_SIZE] = {};
@@ -56,19 +30,18 @@ int main() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (matrix[i][j] == 1 && checked[i][j] != 1) {
-                    s.push(Point(j, i));
+                    s.push(Point(i, j));
                     checked[i][j] = 1;
                     constellars++;
                     int stars = 1;
-                    while (!s.isEmpty()) {
+                    while (!s.empty()) {
                         Point p = s.pop();
                         for (int d = 0; d < 4; d++) {
-                            int x = p.x + dx[d];
-                            int y = p.y + dy[d];
-                            if (matrix[y][x] == 1 && checked[y][x] != 1 && y >= 0 && y < size && x >= 0 && x < size) {
-                                s.push(Point(x, y));
+                            Point n(p.i + di[d], p.j + dj[d]);
+                            if (n.valid(size) && matrix[n.i][n.j] == 1 && !checked[n.i][n.j]) {
+                                s.push(n);
+                                checked[n.i][n.j] = 1;
                                 stars++;
-                                checked[y][x] = 1;
                             }
                         }
                     }
@@ -82,57 +55,4 @@ int main() {
     }
 
     return 0;
-}
-
-template <typename T, unsigned int MAX>
-Stack<T, MAX>::Stack() {
-    this->top = -1;
-}
-
-template <typename T, unsigned int MAX>
-Stack<T, MAX>::~Stack() {
-}
-
-template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isEmpty() {
-    if (this->top == -1)
-        return true;
-    else
-        return false;
-}
-
-template <typename T, unsigned int MAX>
-bool Stack<T, MAX>::isFull() {
-    if (this->top == MAX - 1)
-        return true;
-    else
-        return false;
-}
-
-template <typename T, unsigned int MAX>
-void Stack<T, MAX>::push(T item) {
-    if (this->isFull())
-        return;
-    this->top++;
-    this->items[this->top] = item;
-}
-
-template <typename T, unsigned int MAX>
-T Stack<T, MAX>::pop() {
-    T item;
-    if (isEmpty())
-        return item;
-    item = this->items[this->top];
-    this->top--;
-    return item;
-}
-
-Point::Point() {
-    this->x = 0;
-    this->y = 0;
-}
-
-Point::Point(int _x, int _y) {
-    this->x = _x;
-    this->y = _y;
 }
