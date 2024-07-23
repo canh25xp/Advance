92e044cedba42689e6fb88bfe44e5176bc3fcdbf
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-24 04:21:32 +0700

    .

--- Problems/Hugo_Takes_the_Train/main.cpp
+++ Problems/Hugo_Takes_the_Train/main.cpp
@@ -5,5 +5,12 @@ int main(int argc, char **argv) {
     const char *input = (argc > 1) ? argv[1] : "input.txt";
     freopen(input, "r", stdin);
 
+    int T;
+    cin >> T;
+
+    for (int ti = 0; ti < T; ti++) {
+        int N;
+    }
+
     return 0;
 }
