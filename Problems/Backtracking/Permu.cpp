e107eedb6300dcefd667527d8490386753fdeac0
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-23 20:22:36 +0700

    .

---
+++ Problems/Backtracking/Permu.cpp
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#define MAX_N 1000
+
+using namespace std;
+
+//Dãy N phần tử
+int N = 4;
+int arr[] = { 11,22,33,44 };
+
+//In mảng a với kích thước S
+void printArray(int a[], int S)
+{
+	for (int i = 0; i < S; ++i)
+		cout << a[i] << " ";
+	cout << endl;
+}
+
+//danh sách các phần tử của chỉnh hợp (partial permutation)
+int output[MAX_N];
+
+//visited[i] = true nghĩa là arr[i] đã được đưa vào chỉnh hợp và ngược lại
+bool visited[MAX_N];
+
+int K;
+//Hàm backtrack để sinh tất cả chỉnh hợp chập K của N phần tử
+// Hoán vị là chỉnh hợp chập N của N, muốn in tất cả hoán vị của N phần tử thì cho K = N
+//i: index của phần tử đang check
+//cnt: số lượng phần tử đã được đưa vào chỉnh hợp
+void permutation(int i, int cnt)
+{
+	//Một số trường hợp cắt nhánh sớm, ví dụ tổng hiện tại đã vượt qua minAnswer
+	// if (condition) {
+	// ...
+	// }
+
+	//Đã có đủ K phần tử được đưa vào chỉnh hợp
+	if (cnt == K) {
+		printArray(output, K);
+		return;
+	}
+
+	for (int j = 0; j < N; ++j) {
+		// Nếu arr[j] chưa được đưa vào chỉnh hợp, thử đưa vào và đệ quy với phần tử tiếp theo
+		if (!visited[j]) {
+			visited[j] = true;
+			output[cnt] = arr[j];
+			permutation(i + 1, cnt + 1);
+			visited[j] = false;
+		}
+	}
+}
+
+
+int main()
+{
+	for (int i = 0; i < N; ++i)
+		visited[i] = false;
+        //In tất cả chỉnh hợp chập 2 của dãy 
+	K = 2;
+	permutation(0, 0);
+	cout << endl;
+
+        //In tất cả hoán vị của dãy
+	K = N;
+	permutation(0, 0);
+	return 0;
+}
---
+++ Problems/Backtracking/Subset.cpp
@@ -0,0 +1,98 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+
+#define MAX_N 1000
+
+using namespace std;
+
+//Dãy N phần tử
+int N = 3;
+int arr[] = { 11,22,33 };
+
+//In mảng a với kích thước S
+void printArray(int a[], int S)
+{
+	cout << S << endl;
+	if (S > 0) {
+		for (int i = 0; i < S; ++i)
+			cout << a[i] << " ";
+		cout << endl;
+	}
+}
+
+//danh sách các phần tử của tập con
+int output[MAX_N];
+
+
+//Hàm backtrack để sinh tất cả các tập con
+//i: index của phần tử đang check
+//cnt: số lượng phần tử của tập con
+void subset(int i, int cnt)
+{
+	//Một số trường hợp cắt nhánh sớm, ví dụ tổng hiện tại đã vượt qua minAnswer
+	// if (condition) {
+	// ...
+	// }
+
+	//đã tạo xong 1 tập con, xử lý tập con
+	//trong ví dụ này, in ra số phần tử và danh sách các phần tử của tập con
+	if (i == N) { 
+		printArray(output, cnt);
+		return;
+	}
+	//Không cho arr[i] vào tập con
+	subset(i + 1, cnt);
+
+	//Cho arr[i] vào tập con
+	output[cnt] = arr[i];
+	subset(i + 1, cnt + 1);
+
+}
+
+
+int K;
+//Hàm backtrack để sinh tất cả các tập con có chính xác K phần tử (K <= N)
+//i: index của phần tử đang check
+//cnt: số lượng phần tử của tập con
+void subsetWithK(int i, int cnt)
+{
+	//Một số trường hợp cắt nhánh sớm, ví dụ tổng hiện tại đã vượt qua minAnswer
+	// if (condition) {
+	// ...
+	// }
+
+	//Đã có đủ K phần tử trong tập con
+	if (cnt == K) {
+		printArray(output, K);
+		return;
+	}
+
+	//Trường hợp phải cho hết số phần tử còn lại trong dãy vào tập con
+	if (K - cnt == N - i) {
+		for (int j = i; j < N; ++j)
+			output[cnt++] = arr[j];
+		printArray(output, K);
+		return;
+	}
+
+	//Không cần check i == N vì chắc chắn sẽ vào trường hợp trên trước
+
+	//Không cho arr[i] vào tập con
+	subsetWithK(i + 1, cnt);
+
+	//Cho arr[i] vào tập con
+	output[cnt] = arr[i];
+	subsetWithK(i + 1, cnt + 1);
+}
+
+
+int main()
+{
+	//in tất cả tập con
+	subset(0, 0);
+	//in tất cả tập con có chính xác K phần tử
+	K = 2;
+	subsetWithK(0, 0);
+	return 0;
+}
