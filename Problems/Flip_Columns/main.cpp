af4bb802642f99683266679f6a75c9e604bd8c17
Ngô Văn Cảnh <canh25xp@gmail.com>
2024-07-25 21:36:42 +0700

    .

--- Problems/Flip_Columns/main.cpp
+++ Problems/Flip_Columns/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 
+const int N = 15;
+const int M = 15;
+
+int n, m, k;
+int mat[N][M] = {};
+
+void flip(int j);
+
+int countRows();
+
 using namespace std;
 int main() {
     freopen("input.txt", "r", stdin);
+    cin >> n >> m >> k;
 
-    return 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> mat[i][j];
+
+    int ans = countRows();
+
+    return ans;
+}
+
+void flip(int j) {
+    for (int i = 0; i < n; i++)
+        mat[i][j] = !mat[i][j];
+}
+
+int countRows() {
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        int tmp = 0;
+        for (int j = 0; j < m; j++)
+            tmp += mat[i][j];
+        if (tmp == m)
+            cnt++;
+    }
+
+    return cnt;
 }
